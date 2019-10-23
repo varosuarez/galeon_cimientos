22ada11af2e1acedd1ed97bfbae0ecbcb7c1dc46
Alvaro Suarez <varosuarez@gmail.com>
2019-10-23 18:11:02 +0200

    clase 1

---
+++ Assets/Scripts/Engine/Managers/InputController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Input controller. Clase base para los InputController que se registra automaticamente en el InputMgr
+/// </summary>
+public class InputController : MonoBehaviour {
+
+	// Use this for initialization
+	protected virtual void Awake(){
+        // TODO 1: Res registramos como input especifico para el juego
+        InputMgr inputMgr = GameMgr.GetInstance().GetServer<InputMgr>();
+        if (!inputMgr.IsSetAnyInput())
+        {
+            inputMgr.SetInput(this);
+        }
+        else
+            Debug.LogError("No se puede tener dos instancias de Input en el inputMgr");
+    }
+	
+	protected void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+	}
+	
+	void OnDestroy() 
+	{
+		InputMgr inputMgr = GameMgr.GetInstance().GetServer<InputMgr>();
+		if(inputMgr != null)
+			inputMgr.ClearInput();
+	}
+
+}
---
+++ Assets/Scripts/GameScripts/Galeon/ShowGameMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowGameMenu : MonoBehaviour
+{
+	
+	public string m_menuSceneName;
+	public enum TPushSceneType { PUSH_SCENE, POP_SCENE};
+	public TPushSceneType m_type = TPushSceneType.PUSH_SCENE;
+	public bool m_clearReturnScene = false;
+	
+	protected void Awake()
+	{
+        //TODO 1: nos registras en el input al boton return con OnReturnPressed.
+    }
+	
+	protected void OnReturnPressed()
+	{
+        //muestro el menu..
+        //TODO 2 if type == TPushSceneType.PUSH_SCENE => PushScene else ReturnScene
+        if (m_type == TPushSceneType.PUSH_SCENE)
+            Debug.LogError("apilamos la escena de menu ");
+        else
+            Debug.LogError("Desapilamos la escena en la cima de la pila");
+
+    }
+
+    protected void OnDestroy() 
+	{
+        //TODO 3 desregistrar el return.
+        InputMgr input = GameMgr.GetInstance().GetServer<InputMgr>();
+        if (input != null)
+            Debug.LogError("desregistramos");
+
+    }
+
+    /*protected virtual void Tick(float deltaTime){}
+	protected virtual void Init(){}
+	
+	protected virtual void End() {}*/
+}
